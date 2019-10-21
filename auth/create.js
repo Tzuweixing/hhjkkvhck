4c336fdbb508b5d451128dc17d2719889bb1d33f
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-10-21 15:06:13 -0400

    Update create.js

--- auth/create.js
+++ auth/create.js
@@ -1,11 +1,12 @@
 const userInput = document.getElementById("username");
 const emailInput = document.getElementById("email");
-const passwordInputInput = document.getElementById("password");
+const passwordInput = document.getElementById("password");
 const createButton = document.getElementById("create-button");
 const errorMessage = document.getElementById("error-message");
-createButton.onclick = FUNCTION(){
+
+createButton.onclick = function(){
 const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
-promise.catch(function(erro);{
+promise.catch(function(error);{
               errorMessage.textContent = error.message;
               });
     promise.then(function(){
