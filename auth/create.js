c6b8dc83adb58153d158470c2fdb006b04625034
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-10-21 20:24:27 -0400

    fix error

--- auth/create.js
+++ auth/create.js
@@ -4,13 +4,13 @@ const passwordInput = document.getElementById("password");
 const createButton = document.getElementById("create-button");
 const errorMessage = document.getElementById("error-message");
 
-createButton.onclick = function(){
-const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
-promise.catch(function(error);{
-              errorMessage.textContent = error.message;
-              });
-    promise.then(function(){
+createButton.onclick = function () {
+    const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
+    promise.catch(function (error) {
+        errorMessage.textContent = error.message;
+    });
+    promise.then(function () {
         location.href = "index.html";
-    
+
     });
 };
