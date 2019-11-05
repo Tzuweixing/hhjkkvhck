113c5fca8d4846eed03b9dad2c0aedc309abe34f
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-11-04 21:14:18 -0500

    Update profile.js

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -5,10 +5,10 @@ const profileName = document.getElementById('profile-name');
 const bioInput = document.getElementById('bio');
 const updateButton = document.getElementById('update-profile');
 const profileEmail = document.getElementById('email');
-const email = document.getElementById('Add your email');
+
 
 const profilePhone = document.getElementById('phone');
-const phone = document.getElementById('Phone Number');
+
 
 userRef.on('value', function(snapshot) {
 	const userInfo = snapshot.val();
