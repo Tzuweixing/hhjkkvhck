4c447fcbbd489a6d8a11f8126eeb5ae164d8a389
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-11-06 20:56:51 -0500

    Update profile.js

--- posts/js/profile.js
+++ posts/js/profile.js
@@ -4,6 +4,8 @@ const userRef = firebase.database().ref('users').child(uid);
 const profileName = document.getElementById('profile-name');
 const bioInput = document.getElementById('bio');
 const updateButton = document.getElementById('update-profile');
+const profileEmail = document.getElementById('email');
+const profilePhone = document.getElementById('phone');
 
 userRef.on('value', function(snapshot) {
 	const userInfo = snapshot.val();
@@ -11,12 +13,19 @@ userRef.on('value', function(snapshot) {
 	
 	if (userInfo.bio) {
 		bioInput.value = userInfo.bio;
+		(userInfo.email)
+        emailInput.value = userInfo.email;
+    
+    (userInfo.phone)
+        phoneInput.value = userInfo.phone;
 	}
 });
 
 updateButton.onclick = function() {
 	userRef.update({
 		displayName: profileName.value,
-		bio: bioInput.value
+		bio: bioInput.value,
+		email: emailInput.value,
+        phone: phoneInput.value,
 	});
 };
