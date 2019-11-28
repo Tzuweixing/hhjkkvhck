877ed7468fbf4dfe23d6bf60bcd5064e881e1230
Tzuweixing <44472436+Tzuweixing@users.noreply.github.com>
2019-11-27 19:52:03 -0500

    Add files via upload

--- posts/js/profile.js
+++ posts/js/profile.js
@@ -3,20 +3,23 @@ const userRef = firebase.database().ref('users').child(uid);
 
 const profileName = document.getElementById('profile-name');
 const bioInput = document.getElementById('bio');
-const updateButton = document.getElementById('update-profile');
-const profileEmail = document.getElementById('email');
-const profilePhone = document.getElementById('phone');
+const updateButton = document.getElementById('profile-email');
+const profileEmail = document.getElementById('profile-phone');
+
+
+//const profilePhone = document.getElementById('phone');
+
 
 userRef.on('value', function(snapshot) {
 	const userInfo = snapshot.val();
 	profileName.value = userInfo.displayName;
 	
 	if (userInfo.bio) {
-		bioInput.value = userInfo.bio;
-		(userInfo.email)
-        emailInput.value = userInfo.email;
+		bioInput.value = userInfo.bio;}
+	if(userInfo.email){
+        emailInput.value = userInfo.email;}
     
-    (userInfo.phone)
+    if(userInfo.phone){
         phoneInput.value = userInfo.phone;
 	}
 });
@@ -25,7 +28,7 @@ updateButton.onclick = function() {
 	userRef.update({
 		displayName: profileName.value,
 		bio: bioInput.value,
-		email: emailInput.value,
-        phone: phoneInput.value,
+		profile-email: emailInput.value,
+        profile-phone: phoneInput.value,
 	});
 };
